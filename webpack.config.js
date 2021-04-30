fc4974a73529eef2d05bdab774916bf9ee24b7a9
Teddy Kekana <teddykekana@Teddys-MacBook-Air.local>
2021-04-30 02:14:44 +0200

    feat(GraphQl): Serverless with Apollo with Graphql

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const slsw = require('serverless-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -21,4 +23,14 @@ module.exports = {
         path: path.join(__dirname, '.webpack'),
         filename: '[name].js',
     },
+    plugins: [
+        new CompressionPlugin({
+            algorithm: 'gzip'
+        }),
+        new CopyWebpackPlugin([
+            { copyPermissions: true, from: '.github/**' },
+            { copyPermissions: true, from: 'resolvers/**' },
+            { copyPermissions: true, from: 'schema.js' },
+        ])
+    ]
 };
