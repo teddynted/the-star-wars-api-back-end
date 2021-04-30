a7aa6adbc6531ea377f903a76b2930f8099052df
Teddy Kekana <teddykekana@Teddys-MacBook-Air.local>
2021-04-30 04:42:06 +0200

    feat(GraphQl): Serverless with Apollo with Graphql

--- resolvers/people.js
+++ resolvers/people.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
 const getPeople = (page = 1) => {
+    console.log('page*', page);
     return new Promise((resolve, revoke) => {
         axios.get(`http://swapi.dev/api/people/?page=${page}`).then(({data: {results}}) => {
             resolve(results);
